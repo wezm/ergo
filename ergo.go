0f273efa6aabf15bde4d7b2e79456423e7de1862
Wesley Moore <wez@wezm.net>
2009-11-12 13:55:49 +1100

    Start porting Lessn templates

---
+++ ergo.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag";
+	"http";
+	"io";
+	"log";
+	"strings";
+	"template";
+)
+
+var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
+var fmap = template.FormatterMap{
+	"html": template.HTMLFormatter,
+	"url+html": UrlHtmlFormatter,
+}
+var template_dir = "templates";
+
+func main() {
+	flag.Parse();
+  apptemplate, err := io.ReadFile(template_dir + "/" + "application.html");
+  log.Stdout(template_dir + "/" + "application.html");
+  if err != nil {
+		log.Exit("ReadFile:", err);
+  }
+  var templ = template.MustParse(string(apptemplate), fmap);
+	
+	http.Handle("/", http.HandlerFunc(func(c *http.Conn, req *http.Request) {
+	  QR(c, req, templ)
+	}));
+	http.Handle("/css/", http.FileServer("public/css", "/css/"));
+	http.Handle("/js/", http.FileServer("public/js", "/js/"));
+	
+	err = http.ListenAndServe(*addr, nil);
+	if err != nil {
+		log.Exit("ListenAndServe:", err);
+	}
+}
+
+func QR(c *http.Conn, req *http.Request, templ *template.Template) {
+	templ.Execute(req.FormValue("s"), c);
+}
+
+func UrlHtmlFormatter(w io.Writer, v interface{}, fmt string) {
+	template.HTMLEscape(w, strings.Bytes(http.URLEscape(v.(string))));
+}
+
+
+const templateStr = `
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="viewport" content="width=480" /> 
+<title>QR Link Generator</title>
+<link rel="stylesheet" href="/css/screen.css" type="text/css" media="screen, projection" />
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script src="/js/script.js"></script>
+</head>
+<body>
+{.section @}
+<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={@|url+html}"
+/>
+<br>
+{@|html}
+<br>
+<br>
+{.end}
+<form action="/" name=f method="GET"><input maxLength=1024 size=70
+name=s value="" title="Text to QR Encode"><input type=submit
+value="Show QR" name=qr>
+</form>
+</body>
+</html>
+`
